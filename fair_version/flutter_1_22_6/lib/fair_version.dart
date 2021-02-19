8974b5428f1c71b947efdea6dd3803726a0267f7
ChaobinWu <chaobinwu89@gmail.com>
2021-02-19 17:06:51 +0800

    add flutter 1.22.6; spilt compiler assets

---
+++ fair_version/flutter_1_22_6/lib/fair_version.dart
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2005-present, 58.com.  All rights reserved.
+ * Use of this source code is governed by a BSD type license that can be
+ * found in the LICENSE file.
+ */
+
+export 'src/widgets/all.dart';
+export 'src/widgets/utils.dart';
+export 'src/widgets/version.dart';
+export 'src/widgets/module.dart';
---
+++ fair_version/flutter_1_22_6/lib/src/widgets/$$r.dart
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2005-present, 58.com.  All rights reserved.
+ * Use of this source code is governed by a BSD type license that can be
+ * found in the LICENSE file.
+ */
+
+import 'package:flutter/material.dart';
+
+import 'utils.dart';
+import 'package:flutter/rendering.dart';
+
+var p = () => {
+      'CacheExtentStyle': {
+        'values': CacheExtentStyle.values,
+        'pixel': CacheExtentStyle.pixel,
+        'viewport': CacheExtentStyle.viewport,
+      },
+      'RenderViewport': {
+        'useTwoPaneSemantics': RenderViewport.useTwoPaneSemantics,
+        'excludeFromScrolling': RenderViewport.excludeFromScrolling,
+      },
+      'CrossAxisAlignment': {
+        'values': CrossAxisAlignment.values,
+        'start': CrossAxisAlignment.start,
+        'end': CrossAxisAlignment.end,
+        'center': CrossAxisAlignment.center,
+        'stretch': CrossAxisAlignment.stretch,
+        'baseline': CrossAxisAlignment.baseline,
+      },
+      'FlexFit': {
+        'values': FlexFit.values,
+        'tight': FlexFit.tight,
+        'loose': FlexFit.loose,
+      },
+      'MainAxisAlignment': {
+        'values': MainAxisAlignment.values,
+        'start': MainAxisAlignment.start,
+        'end': MainAxisAlignment.end,
+        'center': MainAxisAlignment.center,
+        'spaceBetween': MainAxisAlignment.spaceBetween,
+        'spaceAround': MainAxisAlignment.spaceAround,
+        'spaceEvenly': MainAxisAlignment.spaceEvenly,
+      },
+      'MainAxisSize': {
+        'values': MainAxisSize.values,
+        'min': MainAxisSize.min,
+        'max': MainAxisSize.max,
+      },
+      'DecorationPosition': {
+        'values': DecorationPosition.values,
+        'background': DecorationPosition.background,
+        'foreground': DecorationPosition.foreground,
+      },
+      'HitTestBehavior': {
+        'values': HitTestBehavior.values,
+        'deferToChild': HitTestBehavior.deferToChild,
+        'opaque': HitTestBehavior.opaque,
+        'translucent': HitTestBehavior.translucent,
+      },
+      'GrowthDirection': {
+        'values': GrowthDirection.values,
+        'forward': GrowthDirection.forward,
+        'reverse': GrowthDirection.reverse,
+      },
+      'SliverGeometry': {
+        'zero': SliverGeometry.zero,
+      },
+      'Overflow': {
+        'values': Overflow.values,
+        'visible': Overflow.visible,
+        'clip': Overflow.clip,
+      },
+      'RelativeRect': {
+        'fill': RelativeRect.fill,
+      },
+      'StackFit': {
+        'values': StackFit.values,
+        'loose': StackFit.loose,
+        'expand': StackFit.expand,
+        'passthrough': StackFit.passthrough,
+      },
+      'PlatformViewHitTestBehavior': {
+        'values': PlatformViewHitTestBehavior.values,
+        'opaque': PlatformViewHitTestBehavior.opaque,
+        'translucent': PlatformViewHitTestBehavior.translucent,
+        'transparent': PlatformViewHitTestBehavior.transparent,
+      },
+      'RenderErrorBox': {
+        'padding': RenderErrorBox.padding,
+        'minimumWidth': RenderErrorBox.minimumWidth,
+        'backgroundColor': RenderErrorBox.backgroundColor,
+        'textStyle': RenderErrorBox.textStyle,
+        'paragraphStyle': RenderErrorBox.paragraphStyle,
+      },
+      'ScrollDirection': {
+        'values': ScrollDirection.values,
+        'idle': ScrollDirection.idle,
+        'forward': ScrollDirection.forward,
+        'reverse': ScrollDirection.reverse,
+      },
+      'SelectionChangedCause': {
+        'values': SelectionChangedCause.values,
+        'tap': SelectionChangedCause.tap,
+        'doubleTap': SelectionChangedCause.doubleTap,
+        'longPress': SelectionChangedCause.longPress,
+        'forcePress': SelectionChangedCause.forcePress,
+        'keyboard': SelectionChangedCause.keyboard,
+        'drag': SelectionChangedCause.drag,
+      },
+      'SystemMouseCursors': {
+        'none': SystemMouseCursors.none,
+        'basic': SystemMouseCursors.basic,
+        'click': SystemMouseCursors.click,
+        'forbidden': SystemMouseCursors.forbidden,
+        'wait': SystemMouseCursors.wait,
+        'progress': SystemMouseCursors.progress,
+        'contextMenu': SystemMouseCursors.contextMenu,
+        'help': SystemMouseCursors.help,
+        'text': SystemMouseCursors.text,
+        'verticalText': SystemMouseCursors.verticalText,
+        'cell': SystemMouseCursors.cell,
+        'precise': SystemMouseCursors.precise,
+        'move': SystemMouseCursors.move,
+        'grab': SystemMouseCursors.grab,
+        'grabbing': SystemMouseCursors.grabbing,
+        'noDrop': SystemMouseCursors.noDrop,
+        'alias': SystemMouseCursors.alias,
+        'copy': SystemMouseCursors.copy,
+        'disappearing': SystemMouseCursors.disappearing,
+        'allScroll': SystemMouseCursors.allScroll,
+        'resizeLeftRight': SystemMouseCursors.resizeLeftRight,
+        'resizeUpDown': SystemMouseCursors.resizeUpDown,
+        'resizeUpLeftDownRight': SystemMouseCursors.resizeUpLeftDownRight,
+        'resizeUpRightDownLeft': SystemMouseCursors.resizeUpRightDownLeft,
+        'resizeUp': SystemMouseCursors.resizeUp,
+        'resizeDown': SystemMouseCursors.resizeDown,
+        'resizeLeft': SystemMouseCursors.resizeLeft,
+        'resizeRight': SystemMouseCursors.resizeRight,
+        'resizeUpLeft': SystemMouseCursors.resizeUpLeft,
+        'resizeUpRight': SystemMouseCursors.resizeUpRight,
+        'resizeDownLeft': SystemMouseCursors.resizeDownLeft,
+        'resizeDownRight': SystemMouseCursors.resizeDownRight,
+        'resizeColumn': SystemMouseCursors.resizeColumn,
+        'resizeRow': SystemMouseCursors.resizeRow,
+        'zoomIn': SystemMouseCursors.zoomIn,
+        'zoomOut': SystemMouseCursors.zoomOut,
+      },
+      'TableCellVerticalAlignment': {
+        'values': TableCellVerticalAlignment.values,
+        'top': TableCellVerticalAlignment.top,
+        'middle': TableCellVerticalAlignment.middle,
+        'bottom': TableCellVerticalAlignment.bottom,
+        'baseline': TableCellVerticalAlignment.baseline,
+        'fill': TableCellVerticalAlignment.fill,
+      },
+      'TextOverflow': {
+        'values': TextOverflow.values,
+        'clip': TextOverflow.clip,
+        'fade': TextOverflow.fade,
+        'ellipsis': TextOverflow.ellipsis,
+        'visible': TextOverflow.visible,
+      },
+      'WrapAlignment': {
+        'values': WrapAlignment.values,
+        'start': WrapAlignment.start,
+        'end': WrapAlignment.end,
+        'center': WrapAlignment.center,
+        'spaceBetween': WrapAlignment.spaceBetween,
+        'spaceAround': WrapAlignment.spaceAround,
+        'spaceEvenly': WrapAlignment.spaceEvenly,
+      },
+      'WrapCrossAlignment': {
+        'values': WrapCrossAlignment.values,
+        'start': WrapCrossAlignment.start,
+        'end': WrapCrossAlignment.end,
+        'center': WrapCrossAlignment.center,
+      },
+    };
