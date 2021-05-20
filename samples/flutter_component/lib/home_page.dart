dc92e7955c98138a403a5f501821fb32b3e10c9d
xiexiongliang <xiexiongliang@58.com>
2021-05-20 10:39:30 +0800

    add flutter_component

---
+++ samples/flutter_component/lib/home_page.dart
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2005-present, 58.com.  All rights reserved.
+ * Use of this source code is governed by a BSD type license that can be
+ * found in the LICENSE file.
+ */
+import 'package:fair/fair.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_compontent/custom_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'theme.dart';
+@FairPatch()
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @FairWell('_counter')
+  int _counter = 0;
+
+  @FairWell('_check')
+  bool _check = false;
+
+  @FairWell('_incrementCounter')
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    _putPhotoCheck();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+              style:  TextStyle(fontSize: 16, color: Colors.black),
+
+            ),
+            Text(
+              '$_counter',
+              style: ThemeStyle.headline4(context),
+
+            ),
+            Text(
+              '$_check',
+              style: ThemeStyle.headline4(context),
+
+            ),
+            CustomWidget(title: "Test",),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+//@FairWell('_putPhotoCheck')
+void _putPhotoCheck() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setBool('survey_permission_check1',true);
+  _getPhotoCheck();
+}
+
+//@FairWell('_getPhotoCheck')
+void _getPhotoCheck() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  _check = prefs.getBool('survey_permission_check1');
+  setState(() {
+    _check = _check;
+  });
+}
+
+}
---
+++ samples/flutter_component/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:fair/fair.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_compontent/src/generated.fair.dart';
+
+import 'delegate.dart';
+import 'module.dart';
+
+
+void main() {
+  runApp(FairApp(
+    child: MyApp(),
+    generated: AppGeneratedModule(),
+    modules: {
+      SlidableModule.tagName:() => SlidableModule()
+    },
+    delegate: {
+      'home_page': (_, data) => MyHomePageDelegate(),
+    },
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FairWidget(
+        name: 'home_page',
+        path: 'assets/bundle/lib_home_page.fair.json',
+        data: {
+          'title': 'Flutter Demo Home Page',
+        },
+      ) /*MyHomePage(title: 'Flutter Demo Home Page')*/,
+    );
+  }
+}
