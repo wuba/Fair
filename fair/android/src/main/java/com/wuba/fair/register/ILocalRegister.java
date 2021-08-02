b40f07fc10aad592f41e3eca1ab466c4f5787932
bujie <bujie@58.com>
2021-08-02 16:57:21 +0800

    - 添加开源代码注释

--- fair/android/src/main/java/com/wuba/fair/register/ILocalRegister.java
+++ fair/android/src/main/java/com/wuba/fair/register/ILocalRegister.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2005-present, 58.com.  All rights reserved.
+ * Use of this source code is governed by a BSD type license that can be
+ * found in the LICENSE file.
+ */
 package com.wuba.fair.register;
 
 public interface ILocalRegister {
