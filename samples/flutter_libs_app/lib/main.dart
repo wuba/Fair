4542a6ae0ffc43c1d537535218143bcea9763bed
linyueyang <linyueyang@58.com>
2021-08-04 18:08:31 +0800

    部分兼容问题修复

--- samples/flutter_libs_app/lib/main.dart
+++ samples/flutter_libs_app/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:fair/fair.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_libs_app/google_fonts/google_font_page.dart';
-import 'package:flutter_libs_app/url_launcher/url_launcher_delegate.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_libs_app/package_info/package_info_delegate.dart';
+import 'package:flutter_libs_app/url_launcher/url_launcher_delegate.dart';
 
 import 'common_delegate.dart';
 import 'device_info/device_info_delegate.dart';
@@ -11,8 +9,9 @@ import 'google_fonts/fair_google_fonts.dart';
 import 'index.dart';
 
 void main() {
-  runApp(FairApp(
-    child: MyApp(),
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FairApp.runApplication(FairApp(
     delegate: {
       'device_info': (_, data) => DeviceInfoDelegate(),
       'url_launcher': (_, data) => UrlLauncherDelegate(),
@@ -20,21 +19,24 @@ void main() {
       'package_info': (_, data) => PackageInfoDelegate(),
     },
     generated: SimpleGenerate(),
-  ));
+    child: MyApp(),
+  ),plugins: {
+    // 'FairNet': FairNet(),
+    // 'WBPermission': WBPermission(),
+    // 'FairPhotoSelector': FairPhotoSelector(),
+  },);
 }
 
-class SimpleGenerate extends GeneratedModule{
+class SimpleGenerate extends GeneratedModule {
   @override
   Map<String, dynamic> components() {
-    return Map<String, dynamic>()
-      ..addAll(google_fonts_component);
+    return Map<String, dynamic>()..addAll(google_fonts_component);
   }
 
   @override
   Map<String, bool> mapping() {
     return Map<String, bool>();
   }
-
 }
 
 class MyApp extends StatelessWidget {
