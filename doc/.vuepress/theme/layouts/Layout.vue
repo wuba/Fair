8188a2284c6e0507f3f86647a870d82d359a8fa8
luozhenglong <luozhenglong@58.com>
2021-08-27 17:10:12 +0800

    公告栏链接

--- doc/.vuepress/theme/layouts/Layout.vue
+++ doc/.vuepress/theme/layouts/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
   <header style="position:fixed;left:0;top:0;width:100%;font-size:14px;line-height:40px;text-align:center;background-color:rgb(217,70,88);color:#ffffff;z-index:20;">
-  Fair 2.0 release！ <a style="color:#4ea2f1" href="./guide/whats_new">What’s new</a>
+  Fair 2.0 release！ <a style="color:#4ea2f1" v-bind:href="whatIsNewUrl">What’s new</a>
   </header>
   <ParentLayout>
   </ParentLayout>
@@ -32,6 +32,11 @@ export default {
   name: 'Layout',
   components: {
     ParentLayout,
+  },
+  computed: {
+    whatIsNewUrl: function() {
+      return (this.$lang == 'zh-CN' ? '/zh' : '') + '/guide/whats_new.html';
+    }
   }
 }
 </script>
