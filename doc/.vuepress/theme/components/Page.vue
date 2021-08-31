eff52a8dbd25efa21ec06d6e3a7e32bad653921c
luozhenglong <luozhenglong@58.com>
2021-08-31 12:07:30 +0800

    修改版权footer

---
+++ doc/.vuepress/theme/components/Page.vue
@@ -0,0 +1,28 @@
+<template>
+  <ParentLayout>
+      <template #bottom>
+          <footer class="cp_footer">Copyright © 2020 58.com | Powered by The Fair Authors</footer>
+      </template>
+  </ParentLayout>
+</template>
+<style>
+.theme-container .page {
+    padding-bottom: 1rem;
+}
+.page .cp_footer {
+    text-align: center;
+    padding: 1rem 0 0 0;
+    font-size: 14px;
+    color: #4e6e8e;
+    border-top: 1px solid #eaecef;
+}
+</style>
+<script>
+import ParentLayout from '@parent-theme/components/Page.vue'
+
+export default {
+  components: {
+    ParentLayout,
+  }
+}
+</script>
