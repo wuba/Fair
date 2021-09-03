54bb3dfef72651e4eae73149de068ac1badd5a91
luozhenglong <luozhenglong@58.com>
2021-09-03 15:29:18 +0800

    dart2js改成直接调用api的方式

--- dart2js/index.dart
+++ dart2js/index.dart
@@ -3,25 +3,9 @@
  * Use of this source code is governed by a BSD type license that can be
  * found in the LICENSE file.
  */
-import 'dart:io';
 import 'dart:async';
+import 'convertor.dart' show convertWidgetStateFile;
 
 Future<String> convertFile(String input, [bool compress = false]) async {
-  var result = await Process.run('which', ['dart']);
-  var bin = StringBuffer();
-  bin.write(result.stdout);
-  var strBin = bin.toString();
-  var dirEndIndex = strBin.lastIndexOf(Platform.pathSeparator);
-  var binDir = strBin.substring(0, dirEndIndex);
-  var args = ['./entry.aot'];
-  if (compress) args.add('--compress');
-  args.add(input);
-  result = await Process.run('$binDir/cache/dart-sdk/bin/dartaotruntime', args);
-
-  return result.stdout.toString();
+  return convertWidgetStateFile(input, compress);
 }
-
-// void main(List<String> args) async {
-//   var res = await convertFile('/Users/luozhenglong/Documents/source/fair-new/fair/example/lib/src/model/part/part_of_sample_page_with_logic.dart');
-//   print(res);
-// }
