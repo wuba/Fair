3ac35aab96b360e74149a01c223a5b6fb3206ff5
chenyouyu <chenyouyu@58.com>
2022-04-28 11:44:08 +0800

    修改samples工程为test_case工程，专门用于放置测试用例

--- samples/fair_bloc/lib/counter/counter.dart
+++
@@ -1,2 +0,0 @@
-export 'cubit/counter_cubit.dart';
-export 'view/counter_page.dart';
--- samples/fair_provider/android/app/src/main/kotlin/com/providerwithfair/demo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.providerwithfair.demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
