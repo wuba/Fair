0e933d45857160ebd3e048010cdaadf2fd80fde7
baixue20 <baixue20@58.com>
2022-06-14 16:12:20 +0800

    在test_case下创建flutter_all_widgets工程

---
+++ test_case/flutter_all_widgets/android/app/src/main/java/com/wuba/flutter_all_widgets/MainActivity.java
@@ -0,0 +1,11 @@
+package com.wuba.flutter_all_widgets;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
