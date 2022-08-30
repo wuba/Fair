2477d50a3fff65b0a202c37382a444e6c1e6c8f9
baixue20 <baixue20@58.com>
2022-08-30 14:38:27 +0800

    ReorderableListView =>v8.V8ScriptCompilationException

--- test_case/flutter_all_widgets/lib/reorderable_listview/reorderable_listview_fair.dart
+++ test_case/flutter_all_widgets/lib/reorderable_listview/reorderable_listview_fair.dart
@@ -39,7 +39,7 @@ class _FairReorderableListViewPageState
     ];
   }
 
-  _onReorder(oldIndex, newIndex) {
+  void _onReorder(oldIndex, newIndex) {
     if (oldIndex < newIndex) {
       newIndex -= 1;
     }
