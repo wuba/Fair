bffa117e31113aa056bf1c0ab22139da0e94bba0
zmtzawqlp <zmtzawqlp@live.com>
2023-05-06 11:16:29 +0800

    1. 增加 baseJsSources 支持用户传递自定义的js 进去 2.去掉 BigInt 自定义方法，BigInt 的兼容性不好

--- fair/assets/fair_core/fair_jsbase.js
+++ fair/assets/fair_core/fair_jsbase.js
@@ -1856,35 +1856,35 @@ Object.prototype.__op_idx__ = function (key) {
   };
   global.String = String;
 })();
-(function () {
-  BigInt.one = 1;
-  BigInt.two = 2;
-  BigInt.zero = 0;
+// (function () {
+//   BigInt.one = 1;
+//   BigInt.two = 2;
+//   BigInt.zero = 0;
 
-  BigInt.from = function (num) {
-    // 取整数部分
-    return parseInt(num);
-  };
+//   BigInt.from = function (num) {
+//     // 取整数部分
+//     return parseInt(num);
+//   };
 
-  BigInt.parse = function (str) {
-    // 只接受数字字符串，小数会报错
-    if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
-      return BigInt(str);
-    } else {
-      throw "FormatException: Could not parse BigInt";
-    }
-  };
+//   BigInt.parse = function (str) {
+//     // 只接受数字字符串，小数会报错
+//     if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
+//       return BigInt(str);
+//     } else {
+//       throw "FormatException: Could not parse BigInt";
+//     }
+//   };
 
-  BigInt.tryParse = function (str) {
-    // 只接受数字字符串，小数会报错
-    if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
-      return BigInt(str);
-    } else {
-      return null;
-    }
-  };
-  global.BigInt = BigInt;
-})();
+//   BigInt.tryParse = function (str) {
+//     // 只接受数字字符串，小数会报错
+//     if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
+//       return BigInt(str);
+//     } else {
+//       return null;
+//     }
+//   };
+//   global.BigInt = BigInt;
+// })();
 (function () {
   const NativeSymbol = global.Symbol;
 
